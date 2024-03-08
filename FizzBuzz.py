1b0eda7ff9a14e8834cdd6d0d972a350010728ae
marjan5126 <marjan15-5126@diu.edu.bd.in>
2024-03-08 21:36:12 +0600

    worng logic

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,11 +1,11 @@
 for i in range(1,100+1):
-     if i%5 == 0:    
-        print("Buzz")
-     elif i%3 == 0:
-        print('Fizz')
-     elif i%3 == 0 and i%5 == 0:    
+      if i%3 == 0 and i%5 == 0:    
         print("FizzBuzz")
-     else:    
+      elif i%5 == 0:    
+        print("Buzz")
+      elif i%3 == 0:
+        print('Fizz')
+      else:    
         print(i)
     
 
